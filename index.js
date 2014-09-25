8da71d65da81b5e85f256baff877bd9f6712fe79
Stefan Buck <web@broud.de>
2014-09-25 20:44:27 +0200

    add flash component

--- index.js
+++ index.js
@@ -10,11 +10,28 @@
 
 var manifest = require('./lib/manifest');
 var reqr = require('./lib/require');
+var flash = require('./lib/flash');
+
+var isNewVersion = function() {
+  var version = '3.0.x';
+  var installedVersion = localStorage.getItem('github-linker-version');
+  if (installedVersion !== version) {
+    localStorage.setItem('github-linker-version', version);
+    if (installedVersion) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = function($, url, cb) {
 
   if ($('.github-linker').length > 0) {
     return cb(null);
+  }
+
+  if (isNewVersion()) {
+    flash($);
   }
 
   if (manifest.supported(url)) {
