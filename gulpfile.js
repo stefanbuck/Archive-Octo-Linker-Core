46cf6d6d8cfc899183c9dbbf9caef056fc77fa03
Stefan Buck <dev@stefanbuck.com>
2014-10-22 07:48:24 +0200

    Handle gulp-plumber errors https://github.com/youngmountain/generator-node-gulp/pull/37

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,20 @@ var paths = {
   source: ['./lib/**/*.js', './index.js']
 };
 
+var onError = function(err) {
+  plugins.util.beep();
+
+  if (process.env.CI) {
+    throw new Error(err);
+  }
+};
+
 gulp.task('lint', function () {
   return gulp.src(paths.lint)
     .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.plumber({
+      errorHandler: onError
+    }))
     .pipe(plugins.jscs())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
@@ -21,8 +32,10 @@ gulp.task('istanbul', function (cb) {
   gulp.src(paths.source)
     .pipe(plugins.istanbul()) // Covering files
     .on('finish', function () {
-      gulp.src(paths.tests)
-        .pipe(plugins.plumber())
+      gulp.src(paths.tests, {cwd: __dirname})
+        .pipe(plugins.plumber({
+          errorHandler: onError
+        }))
         .pipe(plugins.mocha())
         .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
         .on('finish', function() {
@@ -40,8 +53,8 @@ gulp.task('bump', ['test'], function () {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('watch', ['test'], function () {
-  gulp.watch(paths.watch, ['test']);
+gulp.task('watch', ['istanbul'], function () {
+  gulp.watch(paths.watch, ['istanbul']);
 });
 
 gulp.task('test', ['lint', 'istanbul']);
