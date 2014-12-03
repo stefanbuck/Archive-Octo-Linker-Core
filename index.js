e477ed26810d3c194668cd0922bda501b18b1b24
Stefan Buck <dev@stefanbuck.com>
2014-12-03 23:26:07 +0100

    Add options arg
    https://github.com/stefanbuck/github-linker/issues/19

--- index.js
+++ index.js
@@ -8,33 +8,31 @@
 
 'use strict';
 
+var _ = require('lodash');
 var manifest = require('./lib/manifest');
 var reqr = require('./lib/require');
 var flash = require('./lib/flash');
 var utils = require('./lib/utils');
 
-var isNewVersion = function(global) {
-  var version = '3.1.x';
-  var installedVersion = global.localStorage.getItem('github-linker-version');
-  if (installedVersion !== version) {
-    global.localStorage.setItem('github-linker-version', version);
-    if (installedVersion) {
-      return true;
-    }
-  }
-  return false;
-};
+var version = '3.1.x';
 
-module.exports = function(global, $, url, cb) {
+var GitHubLinkerCore = function(global, url, options, cb) {
+  options = options || {};
+  cb = cb || function() {};
+
+  var $ = global.$;
 
   if ($('.github-linker').length > 0) {
     return cb(null);
   }
 
-  if (utils.runInBrowser(global)) {
-    if (isNewVersion(global)) {
-      flash($);
-    }
+  var defaults = {
+    showUpdateNotification: true
+  };
+  this.options = _.defaults(options, defaults);
+
+  if (this.showUpdateNotification() && this.isNewVersion()) {
+    flash($);
   }
 
   if (manifest.supported(url)) {
@@ -42,4 +40,26 @@ module.exports = function(global, $, url, cb) {
   } else if (reqr.supported(url)) {
     reqr.init($, url, cb);
   }
+};
+
+GitHubLinkerCore.prototype.showUpdateNotification = function() {
+  if (this.options.showUpdateNotification && utils.runInBrowser(this.global)) {
+    return true;
+  }
+  return false;
+};
+
+GitHubLinkerCore.prototype.isNewVersion = function() {
+  var installedVersion = this.global.localStorage.getItem('github-linker-version');
+  if (installedVersion !== version) {
+    this.global.localStorage.setItem('github-linker-version', version);
+    if (installedVersion) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports  = function(global, url, options, cb) {
+  new GitHubLinkerCore(global, url, options, cb);
 };
