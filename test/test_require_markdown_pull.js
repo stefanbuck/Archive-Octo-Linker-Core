143a242c56c5603f9a69996f521b4b65e0038ad6
Stefan Buck <github@stefanbuck.com>
2015-01-07 22:27:08 +0000

    add test for pull request view

---
+++ test/test_require_markdown_pull.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('should');
+var _ = require('lodash');
+var helper = require('./helper');
+
+describe('require_markdown pull', function() {
+
+  this.timeout(4000);
+
+  before(function(done) {
+    this.$ = this.result = null;
+
+    helper('require_markdown_pull', 'pull/8/files', function(_jquery, _result) {
+      this.$ = _jquery;
+      this.result = _result;
+      done();
+    }.bind(this));
+  });
+
+  it('found dependencies', function() {
+    this.result.length.should.equal(1);
+  });
+
+  it('check link replacement', function() {
+    this.$('a.github-linker').length.should.equal(1);
+  });
+
+  it('https://github.com/lodash/lodash', function() {
+    var item = _.findWhere(this.result, {
+      name: 'lodash'
+    });
+
+    item.link.should.equal('https://github.com/lodash/lodash');
+
+    item.el.attr('href').should.equal('https://github.com/lodash/lodash');
+    item.el.hasClass('tooltipped').should.be.false;
+  });
+
+});
---
+++ test/test_require_pull.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('should');
+var _ = require('lodash');
+var helper = require('./helper');
+
+describe('require pull', function() {
+
+  this.timeout(4000);
+
+  before(function(done) {
+    this.$ = this.result = null;
+
+    helper('require_pull', 'pull/9/files', function(_jquery, _result) {
+      this.$ = _jquery;
+      this.result = _result;
+      done();
+    }.bind(this));
+  });
+
+  it('found dependencies', function() {
+    // TODO Evaluate why this doesn't work
+    // result.should.have.length(2);
+    this.result.length.should.equal(2);
+  });
+
+  it('http://nodejs.org/api/path.html', function() {
+    var item = _.findWhere(this.result, {
+      name: 'path'
+    });
+
+    item.link.should.equal('http://nodejs.org/api/path.html');
+
+    item.el.attr('href').should.equal('http://nodejs.org/api/path.html');
+    item.el.hasClass('tooltipped').should.be.false;
+  });
+
+  it('https://github.com/lodash/lodash', function() {
+    var item = _.findWhere(this.result, {
+      name: 'lodash'
+    });
+
+    item.link.should.equal('https://github.com/lodash/lodash');
+
+    item.el.attr('href').should.equal('https://github.com/lodash/lodash');
+    item.el.hasClass('tooltipped').should.be.false;
+  });
+});
