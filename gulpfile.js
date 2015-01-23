dc82d4b0d302ca5597233556c8177d6f40df670a
Stefan Buck <github@stefanbuck.com>
2015-01-23 22:22:19 +0100

    bump dep. istanbul

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,18 @@ var paths = {
   source: ['./lib/**/*.js', './index.js']
 };
 
-var onError = function(err) {
-  plugins.util.beep();
+var plumberConf = {};
 
-  if (process.env.CI) {
-    throw new Error(err);
-  }
-};
+if (process.env.CI) {
+  plumberConf.errorHandler = function(err) {
+    throw err;
+  };
+}
 
 gulp.task('lint', function () {
   return gulp.src(paths.lint)
     .pipe(plugins.jshint('.jshintrc'))
-    .pipe(plugins.plumber({
-      errorHandler: onError
-    }))
+    .pipe(plugins.plumber(plumberConf))
     .pipe(plugins.jscs())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
@@ -31,11 +29,10 @@ gulp.task('lint', function () {
 gulp.task('istanbul', function (cb) {
   gulp.src(paths.source)
     .pipe(plugins.istanbul()) // Covering files
+    .pipe(plugins.istanbul.hookRequire()) // Force `require` to return covered files
     .on('finish', function () {
       gulp.src(paths.tests, {cwd: __dirname})
-        .pipe(plugins.plumber({
-          errorHandler: onError
-        }))
+        .pipe(plugins.plumber(plumberConf))
         .pipe(plugins.mocha())
         .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
         .on('finish', function() {
