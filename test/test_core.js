cb6f82912d02d9be4ba25c52f8c5a51a5ecf20e1
Stefan Buck <stefanb@brandwatch.com>
2015-02-06 22:31:51 +0100

    imporve test coverage

---
+++ test/test_core.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('should');
+var core = require('..');
+
+describe('core', function() {
+  var fakeWindow = {
+    document: {
+      getElementById: function() {}
+    }
+  };
+
+  it('require argument window', function () {
+    core.bind(null).should.throw('Missing argument window');
+  });
+
+  it('require option version', function () {
+    core.bind(null, fakeWindow).should.throw('Missing option version');
+  });
+
+  it('require option changelog', function () {
+    core.bind(null, fakeWindow, {version: '1.2.3'}).should.throw('Missing option changelog');
+  });
+});
